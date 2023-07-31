b1645c39fce2e2b46e966ab7e1a32d68601b87c5
3245230002 <140971118+3245230002@users.noreply.github.com>
2023-07-31 05:27:56 +0100

    Add files via upload

---
+++ initials.py
@@ -0,0 +1,30 @@
+# File: initials.py
+# Description: Print out my initials
+# Assignment Number: 1
+#
+# Name: Akyeampong-Mensah Gabriel
+# STUDENT ID:3245230002
+# Email: 3245230002@live.gctu.edu.gh
+#
+# On my honor,Akyeampong-Mensah Gabriel,this programming assignment is my own work
+# and I have not provided this code to any other student
+
+def main():
+    # This function is to print out my initials
+    print()
+    print("...AMG")
+    print()
+    print("........AA.............MMMM....MMMM..........GGGGGGGG.......")
+    print(".......AAAA............MMMM....MMMM.........GG......GG......")
+    print("......AA..AA...........MM.MM..MM.MM........GG........GG.....")
+    print(".....AA....AA..........MM.MM..MM.MM........GG........GG.....")
+    print("....AA......AA.........MM.MM..MM.MM........GG...............")
+    print("...AAAAAAAAAAAA........MM..MMMM..MM........GG..GGGGGGGG.....")
+    print("...AA........AA........MM..MMMM..MM........GG........GG.....")
+    print("...AA........AA........MM...MM...MM........GG........GG.....")
+    print("...AA........AA...**...MM...MM...MM...**....GG......GG....**")
+    print("...AA........AA...**...MM...MM...MM...**.....GGGGGGGG.....**")
+
+
+if __name__ == "__main__":
+    main()
